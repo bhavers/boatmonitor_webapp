90e7caeb06cc92efc937386b5b5609e349373e8d
Bram Havers <bram_havers@nl.ibm.com>
2016-12-31 05:15:12 -0600

    imported old 3d html5 app from poseidon. Also included new v4 d3 line graph example (graph.html)

---
+++ public/js/app.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var sensorsApp = angular.module('sensorsApp', ['ngResource']);
+
+/*sensorsApp.config(['$sceDelegateProvider', function($sceDelegateProvider) {
+         $sceDelegateProvider.resourceUrlWhitelist(['self', 'http://poseidon.mybluemix.net/**']);
+}]);
+*/
+sensorsApp.factory('SensorList', ['$resource',
+  function($resource){
+    return $resource('http://poseidon.mybluemix.net/sensors', {}, {
+      query: {method:'GET', isArray:true}
+    });
+  }]);
+
+var sensorsController = sensorsApp.controller('sensorsController', ['$scope', 'SensorList', function ($scope, SensorList) {
+  $scope.sensors = SensorList.query();  
+}]);
+
